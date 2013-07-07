a509b39d1759e5f18a2c7ab1d02dba5a91148334
Gregory Bataille <gbataille.dev@gmail.com>
2013-07-07 19:17:05 +0200

    Neat alignment with tabular :)

--- db/migrate/20130630091108_create_csp_report_csp_reports.rb
+++ db/migrate/20130630091108_create_csp_report_csp_reports.rb
@@ -1,11 +1,11 @@
 class CreateCspReportCspReports < ActiveRecord::Migration
   def change
     create_table :csp_report_csp_reports do |t|
-      t.string :document_uri, null: false
+      t.string :document_uri       , null: false
       t.string :referrer
       t.string :blocked_uri
-      t.string :violated_directive, null: false
-      t.string :original_policy, null: false
+      t.string :violated_directive , null: false
+      t.string :original_policy    , null: false
 
       t.timestamps
     end
