3fcc72be6ab9f9b9d1f5842a22d51ec62b73acb3
Gregory Bataille <gbataille.dev@gmail.com>
2013-07-12 20:32:34 +0200

    Make the incoming_ip mandatory

--- app/models/csp_report/csp_report.rb
+++ app/models/csp_report/csp_report.rb
@@ -1,5 +1,6 @@
 module CspReport
   class CspReport < ActiveRecord::Base
-    validates_presence_of :document_uri, :violated_directive, :original_policy
+    validates_presence_of :document_uri, :violated_directive, :original_policy,
+      :incoming_ip
   end
 end
