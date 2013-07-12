5553704fcd0bed75429f4040b1e145420defbdb8
Gregory Bataille <gbataille.dev@gmail.com>
2013-07-12 18:13:08 +0200

    Adding the csp_reports/destroy_all route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 CspReport::Engine.routes.draw do
+  # Careful, the destroy_all action needs to be defined BEFORE the resources
+  # indeed the resources declaration add all the csp_reports/XXX routes to be 
+  # the show XXX item action
+  get '/csp_reports/destroy_all' #, controller: 'csp_reports#destroy_all'
   resources :csp_reports
-
 end
