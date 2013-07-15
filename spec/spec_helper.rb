79ed1cf8dcd56117128065505b57f763b76ec7d4
Gregory Bataille <gbataille.dev@gmail.com>
2013-07-15 14:57:21 +0200

    Making the engine routes accessible for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'factory_girl'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -16,6 +17,9 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 FactoryGirl.find_definitions
 
 RSpec.configure do |config|
+  # For engine testing. Makes the Engine routes available to the test files
+  config.include CspReport::Engine.routes.url_helpers
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
