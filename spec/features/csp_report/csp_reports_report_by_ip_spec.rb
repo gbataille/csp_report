f2636c45c5d95dc637679511a995e822c390e00d
Gregory Bataille <gbataille.dev@gmail.com>
2013-08-17 14:12:59 +0200

    Report by IP visualization

---
+++ spec/features/csp_report/csp_reports_report_by_ip_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe 'CspReport report by IP view' do
+  before(:each) do
+    @localhost4   = FactoryGirl.create(:local_inline)
+    @localhost4_2 = FactoryGirl.create(:local_home_index_inline)
+    @localhost6   = FactoryGirl.create(:local_ipv6_home_index_inline)
+    @localhost6_2 = FactoryGirl.create(:local_ipv6_home_index_inline)
+  end
+
+  it 'should display a table of breaches per IP' do
+    visit csp_reports_report_by_ip_path
+    page.should have_content('Incoming IP')
+    page.should have_content('Number of breaches reported')
+  end
+
+  it 'should display each IP only once' do
+    visit csp_reports_report_by_ip_path
+    page.body.scan(@localhost4.incoming_ip).size.should eq 1
+    page.body.scan(@localhost6.incoming_ip).size.should eq 1
+  end
+end
