8ddab96ebd9e96d16becc51d818dec7d357b8bea
Gregory Bataille <gbataille.dev@gmail.com>
2013-09-10 08:40:08 +0200

    Adding factory inheritance for more concise mocks definition

--- spec/factories/csp_report_csp_reports.rb
+++ spec/factories/csp_report_csp_reports.rb
@@ -8,39 +8,20 @@ FactoryGirl.define do
     incoming_ip "127.0.0.1"
   end
 
-  factory :local_home_index_inline, :class => 'CspReport::CspReport' do
+  factory :local_home_index_inline, parent: :local_inline do
     document_uri "http://localhost:3000/home/index"
-    referrer ""
-    blocked_uri ""
-    violated_directive "script-src 'self'"
-    original_policy "script-src 'self'; report-uri /csp/csp_reports"
-    incoming_ip "127.0.0.1"
   end
 
-  factory :local_ipv6_home_index_inline, :class => 'CspReport::CspReport' do
-    document_uri "http://localhost:3000/home/index"
-    referrer ""
-    blocked_uri ""
-    violated_directive "script-src 'self'"
-    original_policy "script-src 'self'; report-uri /csp/csp_reports"
+  factory :local_ipv6_home_index_inline, parent: :local_home_index_inline do
     incoming_ip "::1"
   end
 
-  factory :local_home_about_inline, :class => 'CspReport::CspReport' do
+  factory :local_home_about_inline, parent: :local_inline do
     document_uri "http://localhost:3000/home/about"
-    referrer ""
-    blocked_uri ""
-    violated_directive "script-src 'self'"
-    original_policy "script-src 'self'; report-uri /csp/csp_reports"
-    incoming_ip "127.0.0.1"
   end
 
-  factory :local_script_star, :class => 'CspReport::CspReport' do
-    document_uri "http://localhost:3000"
-    referrer ""
-    blocked_uri ""
+  factory :local_script_star, parent: :local_inline do
     violated_directive "script-src *"
     original_policy "script-src *; report-uri /csp/csp_reports"
-    incoming_ip "127.0.0.1"
   end
 end
